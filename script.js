021459e3e832c54e4231cc34e9fb149e51b4a26a
TheLinguistProgrammer <mikel989989@gmail.com>
2025-10-01 12:05:44 +0300

    Add files via upload
    outline for the html, styling, and json format in js file

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+DATA STRUCTURE FOR CHORDS SHOULD LOOK LIKE THIS:
+
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+{
+    name: "Am",
+    string1: 0;
+    string2: 1;
+    string3: 2;
+    string4: 2;
+    string5: 0;
+    string6: 0;
+}
+
+*/
